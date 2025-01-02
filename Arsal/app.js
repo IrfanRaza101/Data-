6e202245e0b69b4d1765aa70cdd6689be567c6f1
IrfanRaza101 <alaxjoun31@gmail.com>
2025-01-02 19:36:42 +0500

    Update app.js

--- Arsal/app.js
+++ Arsal/app.js
@@ -1,17 +1,17 @@
 document.getElementById("findTable").addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent the default form behavior
-
-    // Collect form data
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
+    
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
     const date = document.getElementById("date").value;
     const time = document.getElementById("time").value;
     const people = document.getElementById("people").value;
 
-    // Check if fields are filled
+
     if (name && email && date && time && people) {
         document.getElementById("outputMessage").innerText = "Table is available!";
+        document.getElementById("outputMessage").style.color = "green";
     } else {
         document.getElementById("outputMessage").innerText = "Please fill all the fields.";
+        document.getElementById("outputMessage").style.color = "red";
     }
 });
